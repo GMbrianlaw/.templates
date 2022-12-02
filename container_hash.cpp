2a65d5b3c4f66f2ab448cf4417dc2d805ce6fbc0
Brian Law <gmbrianlaw@gmail.com>
2022-12-01 22:33:31 -0800

    Modify ContainerHash

--- container_hash.cpp
+++ container_hash.cpp
@@ -18,7 +18,7 @@ public:
         auto result = 0llu;
 
         for (const auto& x : value) {
-            result ^= hash_t(x) + 0x9e3779b9 + (result << 6) + (result >> 2);
+            result ^= hash_t(x) + 0x9e3779b97f4a7c55 + (result << 6) + (result >> 2);
         }
 
         return static_cast<size_t>(result);
