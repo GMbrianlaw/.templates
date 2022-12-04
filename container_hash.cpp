3040a8c369b9290490ecc00bef51bb77305ff8e3
Brian Law <gmbrianlaw@gmail.com>
2022-12-03 23:05:26 -0800

    Change stuff

--- container_hash.cpp
+++ container_hash.cpp
@@ -9,7 +9,7 @@ class ContainerHash {
 
 private:
 
-    const Hash hash_t = Hash();
+    static const auto hash_t = Hash();
 
 public:
 
@@ -18,7 +18,7 @@ public:
         auto result = 0llu;
 
         for (const auto& x : value) {
-            result ^= hash_t(x) + 0x9e3779b97f4a7c55 + (result << 6) + (result >> 2);
+            result ^= hash_t(x) + 0x9e3779b97f4a7c15 + (result << 6) + (result >> 2);
         }
 
         return static_cast<size_t>(result);
