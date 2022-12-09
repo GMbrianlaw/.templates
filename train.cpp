5e2ed015bdca42f859039926ffd530acc945d2d5
Brian Law <gmbrianlaw@gmail.com>
2022-12-08 16:05:27 -0800

    Add train template

---
+++ train.cpp
@@ -0,0 +1,25 @@
+/*
+ID: gmbrian1
+LANG: C++17
+TASK: <TASK_ID>
+*/
+
+#include <fstream>
+
+using std::ifstream;
+using std::ofstream;
+
+auto fin = ifstream(".in");
+auto fout = ofstream(".out");
+
+auto solve() {
+
+}
+
+int main() {
+
+    solve();
+
+    return 0;
+
+}
