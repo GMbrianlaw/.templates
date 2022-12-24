a106378fbf9c18dda936e3ad4817f5bd489fc4dd
Brian Law <gmbrianlaw@gmail.com>
2022-12-24 12:48:09 -0800

    Add `factorial` template

---
+++ utility/factorial.cpp
@@ -0,0 +1,13 @@
+// fold 11j
+template <typename IntegralType>
+auto factorial(int n) {
+
+    auto result = IntegralType(1);
+
+    for (auto i = 2; i <= n; ++i) {
+        result *= i;
+    }
+
+    return result;
+
+}
