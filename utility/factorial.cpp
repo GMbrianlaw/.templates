f2350066fc62bc06654d4f0552934b87f7b65a06
Brian Law <gmbrianlaw@gmail.com>
2022-12-24 12:53:04 -0800

    Fix some stuff

--- utility/factorial.cpp
+++ utility/factorial.cpp
@@ -5,7 +5,7 @@ auto factorial(int n) {
     auto result = IntegralType(1);
 
     for (auto i = 2; i <= n; ++i) {
-        result *= i;
+        result *= IntegralType(i);
     }
 
     return result;
