a18ce9077eefaaaacf5b644681e2ab0daf974857
Brian Law <gmbrianlaw@gmail.com>
2023-01-06 16:05:33 -0800

    Make changes

---
+++ factorize.cpp
@@ -0,0 +1,21 @@
+#include <vector>
+
+// fold 17j
+auto factorize(int number) {
+
+    const auto bits = __builtin_ctz(number);
+
+    auto factors = std::vector<int>(bits, 2);
+
+    number >>= bits;
+
+    for (auto i = 3; i * i <= number; ++i) {
+        while (number % i == 0) {
+            factors.push_back(i);
+            number /= i;
+        }
+    }
+
+    return factors;
+
+}
