2a2795658156bbae2e05c40257d34ea2fc1e2631
Brian Law <gmbrianlaw@gmail.com>
2023-01-08 16:08:31 -0800

    Add debug container

--- debug.cpp
+++ debug.cpp
@@ -16,3 +16,19 @@ auto debug(Type value, Rest... rest) {
     debug(rest...);
 
 }
+
+template <typename Type>
+auto debug_container(const Type& value) {
+
+    const auto value_sz = static_cast<int>(value.size());
+
+    for (auto i = 0; i < value_sz; ++i) {
+        std::clog << value[i];
+        if (i < value_sz - 1) {
+            std::clog << ' ';
+        } else {
+            std::clog << std::endl;
+        }
+    }
+
+}
